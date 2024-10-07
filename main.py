f289aa4318728b77ed8d995133cff36031f75009
ne0ekspert <rlaaudwk5503@gmail.com>
2024-10-07 10:18:34 +0900

    start browser after server starts

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import subprocess
 from dotenv import load_dotenv
 import logging
 from pathlib import Path
+import asyncio
 from aiohttp import web
 from websocket import ws_voice, ws_prod, ws_nfc
 
@@ -15,6 +17,8 @@ if not Path('voice-activated-kiosk-ui/build').is_dir():
     os.system("npm run build")
     os.chdir("..")
 
+loop = asyncio.new_event_loop()
+
 app = web.Application()
 logging.basicConfig(level=logging.DEBUG)
 async def index(request):
@@ -27,7 +31,20 @@ app.router.add_get('/ws/prod', ws_prod)
 app.router.add_get('/ws/nfc', ws_nfc)
 app.router.add_get('/{path:.*}', index)
 
+fake_future = asyncio.Future(loop=loop)
+def run_browser(app):
+    return loop.create_task(loop.subprocess_exec(asyncio.SubprocessProtocol, 
+        'x-www-browser',
+        'http://localhost:8080',
+        '--start-maximized',
+        '--start-fullscreen',
+        '--kiosk',
+        '--disable-html5-camera',
+        '--disk-cache-dir=/dev/null',
+        '--disk-cache-size=1'
+    ))
+
 if __name__ == '__main__':
-    print("서버 시작!")
-    web.run_app(app)
-    
+    app.on_startup.append(run_browser)
+
+    web.run_app(app, loop=loop)
--- run-kiosk.sh
+++ run-kiosk.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 export GOOGLE_APPLICATION_CREDENTIALS='/home/pi/license.json'
+export DISPLAY=:0
 cd '/home/pi/voice-activated-kiosk'
-x-www-browser http://localhost:8080 --start-maximized --start-fullscreen --kiosk &
+
 /home/pi/venv/bin/python3 main.py
