8a153fd2e0eb9611c33c062ce3981185c31a02c5
ne0ekspert <rlaaudwk5503@gmail.com>
2024-10-07 10:29:01 +0900

    gcloud tts

---
+++ gcloud_tts.py
@@ -0,0 +1,20 @@
+from google.cloud import texttospeech
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+voice = texttospeech.VoiceSelectionParams(
+    language_code="ko-KR", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+)
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
+
+def synthesis(text):
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    with open("temp.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
