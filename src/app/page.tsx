23b88702f09b9426c44c61609c575df027a04709
ne0ekspert <rlaaudwk5503@gmail.com>
2024-11-21 21:14:42 +0900

    startup screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
+import './animation.css';
+// start_screen/bur.jpg (이미지 위치)
+// /public/static/start_screen/bur.jpg
 export default function Home() {
   return (
     <div>
-      <h2>Welcome to the Kiosk</h2>
-      <Link href="/order">Go to Menu</Link>
-      <Link href="/checkout">Go to Checkout</Link>
+      <div className="hero">
+        <Image src="/static/start_screen/bur.jpg" alt="Burrito Background" fill />
+        <div className="hero-text">
+          <h1 className="animated-text">Welcome to Sandstone Café</h1>
+          <p>Order your favorite meal online and enjoy!</p>
+          <Link href="/order">Start Order</Link>
+        </div>
+      </div>
     </div>
   );
 }
